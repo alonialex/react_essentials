d518e258a1711cb9da5a8df3fec45f3a11e8ac25
alonialex <alexnati07@gmail.com>
2024-07-11 18:23:56 +0300

    Working with use effect

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -10,16 +10,21 @@ import { useState, useEffect } from 'react'
 
 function App() {
   const[emotion,setEmotion] = useState("happy")
-
+  const[secondary,setSecondary] = useState("tired")
   useEffect(() =>{
     console.log(`It is ${emotion} here`)
   },[emotion])
+
+  useEffect(() =>{
+    console.log(`It is ${secondary} here`)
+  },[secondary])
   return (
     <>
     <p>Current emotion is {emotion}</p>
     <button onClick={() => setEmotion("Sad")}>Sad</button>
     <button onClick={() => setEmotion("Excited")}>Excited</button>
-
+    <p>Current emotion is {secondary}</p>
+    <button onClick={() => setSecondary("Grateful")}>Grateful</button>
     </>
   )
 }
